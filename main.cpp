bfd2c518c98604931f3a48e115dc82e196e76d41
CHizzyEzu <155769930+CHizzyEzu@users.noreply.github.com>
2024-02-21 16:56:47 -0500

    Changed grammar and spacing conflicts

--- main.cpp
+++ main.cpp
@@ -9,33 +9,33 @@ using namespace std;
 
 int main()
 {
-// Double Variables and combinations
-    
+    // Double Variables and combinations
+
     double loan, rate, pay, mon, apb, ip;
-        
-    
-// Asking for Loan amount, rate % and amount of payments
-    
+
+
+    // Asking for Loan amount, rate % and amount of payments
+
     cout << "Enter the amount you owe:  $";
     cin >> loan;
     cout << "Enter your rate percentage:  %";
     cin >> rate;
     cout << "Enter the amount of payments made:  ";
     cin >> pay;
-    
-// Calculatations
-        rate = rate / 100;
-        mon = (rate * pow(1 + rate, pay)) / (pow(1 + rate, pay) - 1) * loan;
-        apb = (mon * 12) + loan;
-        ip = mon * 12;
+
+    // Calculatations
+
     rate = rate / 100;
-             
-// Output Code and Stream Manipulation
-             
+    mon = (rate * pow(1 + rate, pay)) / (pow(1 + rate, pay) - 1) * loan;
+    apb = (mon * 12) + loan;
+    ip = mon * 12;
+    rate = rate / 100;
+
+    // Output Code and Stream Manipulation
+
     cout << setprecision(2) << fixed;
-    cout << "Monthly Payment:  " << setw(6) << mon << endl;
-    cout << "Amount Paided Back:  " << setw(6) << apb << endl;
-    cout << "Interest Paid:  " << setw(6) << ip << endl;
-    
+    cout << "Monthly Payment:  " << setw(10) << mon << endl;
+    cout << "Amount Paid Back:  " << setw(10) << apb << endl;
+    cout << "Interest Paid:  " << setw(10) << ip << endl;
+
     return 0;
-}
